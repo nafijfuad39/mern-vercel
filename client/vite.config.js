58ac3923d744b19260d1ba5b51f8da3d64f9eff0
nafijfuad39 <nafijfuad39@gmail.com>
2025-01-06 22:23:41 +0600

    12 commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/api/':{
-        target:"https://mern-vercel-ashy.vercel.app"
-      }
-    }
-  }
 })
