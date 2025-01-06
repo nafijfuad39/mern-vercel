070f49b2c24ba2f27d363cf1812e6c01b9e4570e
nafijfuad39 <nafijfuad39@gmail.com>
2025-01-06 22:09:53 +0600

    10th  commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api/':{
-        target:"http://localhost:7000"
+        target:"https://mern-vercel-ashy.vercel.app"
       }
     }
   }
